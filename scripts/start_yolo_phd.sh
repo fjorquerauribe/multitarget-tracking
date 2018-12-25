582e25ee6260701d241e18668ab4f8f9f41c0652
sergio <sherna90@yahoo.es>
2018-12-25 10:40:19 -0300

    fixed yolo nms

--- scripts/start_yolo_phd.sh
+++ scripts/start_yolo_phd.sh
@@ -1,1 +1,1 @@
-../build/yolo -img data/$1/$2/$3/img1/000001.jpg -gt data/$1/$2/$3/gt/gt.txt -config ./yolov3.cfg -model ./yolov3.weights  -classes ./coco.names -min_confidence 0.95 -epsilon $4 -verbose $5
+../build/yolo -img data/$1/$2/$3/img1/000001.jpg -gt data/$1/$2/$3/gt/gt.txt -config ../build/yolov3.cfg -model ../build/yolov3.weights  -classes ../build/coco.names -min_confidence 0.95 -epsilon $4 -verbose $5
